d3e9d1622b4642ffe870a22bb964d0be03a56669
Mario Antonio López Ruiz <marioanloru@gmail.com>
2018-11-02 21:16:54 +0100

    Arreglando despliegue a heroku utilizando grunt

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ const app = require('./app.js')
 
 //  Server listening on the specified port
 app.listen(process.env.PORT || 8333, () => {
-    console.log('Listening on port 8333')
+    let puerto;
+    if (process.env.PORT) puerto = process.env.PORT
+    else puerto = 8333 
+    console.log('Listening on port', puerto);
 });
