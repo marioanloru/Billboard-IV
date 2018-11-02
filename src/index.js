575c816a04ec57ce689f91481c2ceec0f4845b8a
Mario Antonio López Ruiz <marioanloru@gmail.com>
2018-11-02 21:05:00 +0100

    Modificado gitignore, arreglado bindeo de puerto para despliegue en Heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app.js')
 
 //  Server listening on the specified port
-app.listen(8333, () => {
+app.listen(process.env.PORT || 8333, () => {
     console.log('Listening on port 8333')
 });
